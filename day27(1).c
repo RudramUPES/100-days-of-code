e19ad5e674da6bdc9d2912b7d63f829c12438459
Rudram Sharma <rudram2505@gmail.com>
2025-11-22 23:19:43 +0530

    Create day27(1).c

---
+++ day27(1).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, n = 5; // n controls the peak line length
+    // Top half including the middle line
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    // Bottom half
+    for(i = n - 1; i >= 1; i--) {
+        for(j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
